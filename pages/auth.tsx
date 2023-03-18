f725fd67168f3ffd490b4ce2722926024195c15e
Jweb60 <jy.babiarczyk@gmail.com>
2023-03-18 13:41:41 +0100

    second commit

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Input from "@/components/Input";
+
+
+
 const Auth = () => {
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, SetPassword] = useState("");
+  
+  
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50 ">
@@ -8,10 +19,29 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign-In
+              S'identifier
             </h2>
             <div className="flex flex-col gap-4">
-              
+              <Input 
+                label="Username"  
+                onChange={(e: any) => setName(e.target.value)}
+                id="name"                
+                value={name}
+              />
+                <Input 
+                label="E-mail"  
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+                <Input 
+                label="Mot de passe"  
+                onChange={(e: any) => SetPassword(e.target.value)}
+                id="password"
+                type="password"
+                value={email}
+              />
             </div>
           </div>
         </div>
