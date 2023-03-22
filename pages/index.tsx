d0d091691348356cde898fcd3341cb1461115753
Jweb60 <jy.babiarczyk@gmail.com>
2023-03-22 07:45:22 +0100

    env commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
-
-
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
-  
   const session = await getSession(context);
 
   if (!session) {
@@ -22,13 +21,18 @@ export async function getServerSideProps(context: NextPageContext) {
     props: {},
   };
 }
-  
 
 export default function Home() {
-    return (
-      <>
+
+  const { data: movies = [] } = useMovieList();
+  
+  return (
+    <>
       <Navbar />
       <Billboard />
-      </>
-    )
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
+    </>
+  );
 }
